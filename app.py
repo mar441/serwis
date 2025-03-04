0d9f9ddaede4b63d290256162591051d930aa28f
Maria <79464091+mar441@users.noreply.github.com>
2025-03-04 12:27:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -422,7 +422,7 @@ app.layout = html.Div([
             dbc.ModalHeader(),
             dbc.ModalBody(
                 html.Iframe(
-                    src="https://drive.google.com/file/d/150n_kY5mgsTAskhlYIBf1bu67y6hVeE8/view?usp=sharing",
+                    src="https://drive.google.com/file/d/1rqgJItrSTVqV18ZzcbUYAuQQD9hO4EjE/view?usp=sharing",
                     style={"height": "500px", "width": "100%"}
                 )
             ),
